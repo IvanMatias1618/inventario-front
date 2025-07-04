8a2e92140c39325e6651a099d1253973d198cb66
IvanMatias1618 <tacolopez1618033@outloo.com>
2025-07-04 15:34:42 -0600

    empezando entidades

--- src/despliegue_de_entidades.tsx
+++ src/despliegue_de_entidades.tsx
@@ -1,31 +1,44 @@
 import type { Insumo, InsumoValor, InsumoEditado } from "./contratos/tipos";
 import { servicioDeInsumos } from "./contratos/servicio";
 
-export function Despliegue_de_entidades(){
+export function DespliegueDeEntidades(){
+  let todos = servicioDeInsumos.listar();
+  let insumos: InsumoValor[];
+  for insumo in todos{
+    let nuevo_insumo = servicioDeInsumos.valorInsumo(insumo);
+    insumos.add(nuevo_insumo);
+  }
+  const listaInsumos = insumos.map(insumo => Entidades(insumo));
   return (
     <>
-      
+      <div id="barra_entidades"> <ul> {listaInsumos} </ul>
+      </div> 
     </>
   );
 }
 
-function Entidades(){
+function Entidades(insumo: InsumoValor){
+  let info = `Cantidad: ${insumo.cantidad}, \n Cantidad minima: ${insumo.cantidad_minima}.\n costo: ${insumo.precio}`;
   return (
     <div>
-      <img></img>
+      <img src=imagen ></img> //agregar una funcion y lista de imagenes con el nombre de cada entidad para renderizar aqui con su src
       <div className="datos">
-        <h1 className="insumo_nombre"> { insumo }</h1>
-        <h1 className="descricion"> Descripcion del insumo: id: nombre: cantidad: cantidad minima: costo:
+        <h1 className="insumo_nombre"> { insumo.nombre } </h1>
+        <h1 className="descricion"> {info}
         </h1> 
         </div>
-      
-       
     </div>
   );
 }
 
 
-//CONTRATOS: ??
+
+
+function Buscador(){
+  return (
+    
+  );
+}
 //Buscador
 //
 // Entidades
